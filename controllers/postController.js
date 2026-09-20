da0d350f5b37f4cfc43342d6339438af92fbdce9
suhel khan8080@gmail.com <suhailali8080@gmail.com>
2026-09-20 21:49:34 +0530

    Create Comment

---
+++ controllers/postController.js
@@ -0,0 +1,26 @@
+const Post = require("../models/postModel");
+
+exports.createPost = async(req,res) => {
+    try{
+
+        const {title, body} = req.body;
+        const post = new Post({
+            title, body,
+        });
+        const savedPost = await post.save();
+
+        res.json({
+            post: savedPost,
+        })
+
+    }
+
+    catch(error){
+
+        return res.status(400).json({
+            error: "Enter while creating post",
+        });
+
+    }
+    
+}
--- routes/blog.js
+++ routes/blog.js
@@ -8,13 +8,13 @@ const router = express.Router();
 
 const { dummyLink } = require("../controllers/LikeController");
 const {createComment} = require("../controllers/CommentController");
-
-
+const{ createPost} = require("../controllers/PostController");
+ 
 
 //Mapping create
 router.get("/dummyroute", dummyLink );
 router.post("/comments/create", createComment);
-
+router.post("/posts/create", createPost)
 
 //export
 module.exports = router;
